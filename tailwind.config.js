217f4788eefbb4558a03df2485e687c44e8d8d87
Irina Spasova <irina.spasova.81@gmail.com>
2022-08-24 19:32:02 +0300

    hero ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
         muted: "#C9CBCF",
         accent: "#07693A",
       },
+      dropShadow: {
+        "text-sm": "1px 1px 5px rgba(0, 0, 0, 0.90)",
+        "text-md": "1px 2px 5px rgba(0, 0, 0, 0.90)",
+        "text-lg": "1px 4px 5px rgba(0, 0, 0, 0.90)",
+      },
       container: {
         center: true,
         padding: {
